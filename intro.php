d4620807bbf673157baa4e9696bf0e92d605270a
Hassan-92 <hassan.ajeg@hotmail.fr>
2020-04-03 14:45:44 +0200

    ajout de l'into

---
+++ intro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: <?php echo $_GET["color"]?> ;
+        }
+    </style>
+</head>
+<body>
+    <h1>Bonjour !</h1>
+<?php
+    
+    $now = date('d/m/Y');
+    $name = $_GET["name"];
+    echo "Bonjour  {$name}" ;
+    echo "<br> nous sommes le : ";
+    echo $now;
+
+?>
+<pre>
+    <?php
+
+    var_dump($_SERVER);
+
+
+?>
+</pre>
+    
+</body>
+</html>
